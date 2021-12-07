1dedcafcac648d9ccf2632508756e0c01dad5deb
Brian Rui <Brian.Rui01@gmail.com>
2021-12-06 19:31:38 -0500

    Add tests

--- tests/tests.ml
+++ tests/tests.ml
@@ -55,7 +55,36 @@ let test_place_ship _ =
    ^ " E(9,F)  E(9,G)  E(9,H)  E(9,I)  E(9,J) \n")
   @@ B.board_to_string test_board;
   assert_equal None
-  @@ B.place_ship Battleship 4 B.create_board ((0, 'M'), B.Horizontal)
+  @@ B.place_ship Battleship 4 B.create_board ((0, 'M'), B.Horizontal);
+  let test_board =
+    match B.place_ship Patrol 2 B.create_board ((0, 'A'), B.Vertical) with
+    | Some board -> board
+    | None -> B.create_board
+  in
+  assert_equal
+    (" O(0,A)  E(0,B)  E(0,C)  E(0,D)  E(0,E) "
+   ^ " E(0,F)  E(0,G)  E(0,H)  E(0,I)  E(0,J) \n"
+   ^ " O(1,A)  E(1,B)  E(1,C)  E(1,D)  E(1,E) "
+   ^ " E(1,F)  E(1,G)  E(1,H)  E(1,I)  E(1,J) \n"
+   ^ " E(2,A)  E(2,B)  E(2,C)  E(2,D)  E(2,E) "
+   ^ " E(2,F)  E(2,G)  E(2,H)  E(2,I)  E(2,J) \n"
+   ^ " E(3,A)  E(3,B)  E(3,C)  E(3,D)  E(3,E) "
+   ^ " E(3,F)  E(3,G)  E(3,H)  E(3,I)  E(3,J) \n"
+   ^ " E(4,A)  E(4,B)  E(4,C)  E(4,D)  E(4,E) "
+   ^ " E(4,F)  E(4,G)  E(4,H)  E(4,I)  E(4,J) \n"
+   ^ " E(5,A)  E(5,B)  E(5,C)  E(5,D)  E(5,E) "
+   ^ " E(5,F)  E(5,G)  E(5,H)  E(5,I)  E(5,J) \n"
+   ^ " E(6,A)  E(6,B)  E(6,C)  E(6,D)  E(6,E) "
+   ^ " E(6,F)  E(6,G)  E(6,H)  E(6,I)  E(6,J) \n"
+   ^ " E(7,A)  E(7,B)  E(7,C)  E(7,D)  E(7,E) "
+   ^ " E(7,F)  E(7,G)  E(7,H)  E(7,I)  E(7,J) \n"
+   ^ " E(8,A)  E(8,B)  E(8,C)  E(8,D)  E(8,E) "
+   ^ " E(8,F)  E(8,G)  E(8,H)  E(8,I)  E(8,J) \n"
+   ^ " E(9,A)  E(9,B)  E(9,C)  E(9,D)  E(9,E) "
+   ^ " E(9,F)  E(9,G)  E(9,H)  E(9,I)  E(9,J) \n")
+  @@ B.board_to_string test_board;
+  assert_equal None
+  @@ B.place_ship Battleship 4 test_board ((0, 'A'), B.Vertical)
 
 let test_attack_cell _ =
   let test_board =
@@ -89,7 +118,7 @@ let test_attack_cell _ =
   assert_equal B.Success attack_result;
   let _, sunk_ships = result_board in
   assert_equal 0 @@ List.length sunk_ships;
-  let result_board', attack_result' = B.attack_cell result_board (0, 'B') in
+  let result_board, attack_result = B.attack_cell result_board (0, 'B') in
   assert_equal
     (" H(0,A)  H(0,B)  E(0,C)  E(0,D)  E(0,E) "
    ^ " E(0,F)  E(0,G)  E(0,H)  E(0,I)  E(0,J) \n"
@@ -111,9 +140,9 @@ let test_attack_cell _ =
    ^ " E(8,F)  E(8,G)  E(8,H)  E(8,I)  E(8,J) \n"
    ^ " E(9,A)  E(9,B)  E(9,C)  E(9,D)  E(9,E) "
    ^ " E(9,F)  E(9,G)  E(9,H)  E(9,I)  E(9,J) \n")
-  @@ B.board_to_string result_board';
-  assert_equal B.Success attack_result';
-  let _, sunk_ships' = result_board' in
+  @@ B.board_to_string result_board;
+  assert_equal B.Success attack_result;
+  let _, sunk_ships = result_board in
   assert_equal
     [
       {
@@ -123,7 +152,37 @@ let test_attack_cell _ =
         orientation = Horizontal;
       };
     ]
-    sunk_ships'
+    sunk_ships;
+  let result_board, attack_result = B.attack_cell result_board (0, 'C') in
+  assert_equal
+    (" H(0,A)  H(0,B)  M(0,C)  E(0,D)  E(0,E) "
+   ^ " E(0,F)  E(0,G)  E(0,H)  E(0,I)  E(0,J) \n"
+   ^ " E(1,A)  E(1,B)  E(1,C)  E(1,D)  E(1,E) "
+   ^ " E(1,F)  E(1,G)  E(1,H)  E(1,I)  E(1,J) \n"
+   ^ " E(2,A)  E(2,B)  E(2,C)  E(2,D)  E(2,E) "
+   ^ " E(2,F)  E(2,G)  E(2,H)  E(2,I)  E(2,J) \n"
+   ^ " E(3,A)  E(3,B)  E(3,C)  E(3,D)  E(3,E) "
+   ^ " E(3,F)  E(3,G)  E(3,H)  E(3,I)  E(3,J) \n"
+   ^ " E(4,A)  E(4,B)  E(4,C)  E(4,D)  E(4,E) "
+   ^ " E(4,F)  E(4,G)  E(4,H)  E(4,I)  E(4,J) \n"
+   ^ " E(5,A)  E(5,B)  E(5,C)  E(5,D)  E(5,E) "
+   ^ " E(5,F)  E(5,G)  E(5,H)  E(5,I)  E(5,J) \n"
+   ^ " E(6,A)  E(6,B)  E(6,C)  E(6,D)  E(6,E) "
+   ^ " E(6,F)  E(6,G)  E(6,H)  E(6,I)  E(6,J) \n"
+   ^ " E(7,A)  E(7,B)  E(7,C)  E(7,D)  E(7,E) "
+   ^ " E(7,F)  E(7,G)  E(7,H)  E(7,I)  E(7,J) \n"
+   ^ " E(8,A)  E(8,B)  E(8,C)  E(8,D)  E(8,E) "
+   ^ " E(8,F)  E(8,G)  E(8,H)  E(8,I)  E(8,J) \n"
+   ^ " E(9,A)  E(9,B)  E(9,C)  E(9,D)  E(9,E) "
+   ^ " E(9,F)  E(9,G)  E(9,H)  E(9,I)  E(9,J) \n")
+  @@ B.board_to_string result_board;
+  assert_equal B.Missed attack_result;
+  let _, attack_result = B.attack_cell result_board (11, 'B') in
+  assert_equal B.Invalid attack_result;
+  let _, attack_result = B.attack_cell result_board (0, 'A') in
+  assert_equal B.Repeat attack_result;
+  let _, attack_result = B.attack_cell result_board (0, 'C') in
+  assert_equal B.Repeat attack_result
 
 let test_is_game_over _ = assert_equal false @@ B.is_game_over B.create_board
 
